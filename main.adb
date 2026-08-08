36fff2cb669a4eb26240931063b669851674b3a5
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-08 18:34:25 +0200

    Create main.adb

---
+++ main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("Ricart-Agrawala Algorithm initialized.");
+   Put_Line ("Please run 'make test' to execute the V&V test suite.");
+end Main;
